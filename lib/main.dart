343bfffc8e5ffbc2c2a85e5aa941a2f377bc3b1b
samuelematias <hi@samuelematias.com>
2020-06-24 12:32:06 -0300

    implemented ChangeNotifierProvider/Consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:theming/app_state.dart';
 import 'package:theming/app_theme.dart';
 import 'package:theming/task_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+      ChangeNotifierProvider<AppState>(
+        create: (context) => AppState(),
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Theming',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: TaskPage(),
+    return Consumer<AppState>(
+      builder: (context, appState, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Theming',
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: appState.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+          home: TaskPage(),
+        );
+      },
     );
   }
 }

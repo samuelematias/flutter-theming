877d38ff709dd158dcc5c26cddf0a202b0ee20b1
samuelematias <hi@samuelematias.com>
2020-06-24 11:33:59 -0300

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:theming/theming_page.dart';
+import 'package:theming/task_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Theming',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: ThemingPage(),
+      home: TaskPage(),
     );
   }
 }
